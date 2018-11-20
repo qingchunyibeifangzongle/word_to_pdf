95198228f8700b87dc469b5c93a9646dcaf64a27
qingchunyibeifangzongle <995645653@qq.com>
2018-11-20 17:35:46 +0800

    两种方式测试

---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuepeng
+ * Date: 2018/11/20
+ * Time: 下午5:33
+ */
+
+
+
+
+function test()
+{
+    /* $file = "/Users/yuepeng/Desktop/开放平台额度扩展需求申请函 (1).docx";
+     $phpWord = \PhpOffice\PhpWord\IOFactory::load($file);
+     $html = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord  ,'HTML');
+     $html->save('/Users/yuepeng/Desktop/开放平台额度扩展需求申请函.html');
+
+
+
+
+     $html1 = file_get_contents('/Users/yuepeng/Desktop/开放平台额度扩展需求申请函.html');
+
+
+     $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+     $pdf->SetCreator(PDF_CREATOR);
+
+
+
+     // 设置自动换页
+     $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+     // 设置图像比例因子
+     $pdf->setImageScale(1.25);
+     // 设置默认字体构造子集模式
+     $pdf->setFontSubsetting(true);
+
+     // 设置字体 stsongstdlight支持中文
+     $pdf->SetFont('stsongstdlight', '', 14);
+
+     // 添加一页
+     $pdf->AddPage();
+
+     $pdf->Ln(5);//换行符
+
+     $pdf->writeHTML($html1 ,true, false, true, false, '');
+     $pdf->Output('tt.pdf', 'I');*/
+
+
+    $doc = '/Users/yuepeng/Desktop/电子签/1.天印电子签章私有云产品白皮书_V1.1.docx';
+    $pdf = '/Users/yuepeng/Desktop/天印电子签章私有云产品白皮书1.pdf';
+//        export LANG=en_US.UTF-8;/usr/bin/java -jar /data/wwwroot/PDFconvert/jodconverter-2.2.2/lib/jodconverter-cli-2.2.2.jar {word_file} {pdf_file}
+
+    $size = filesize($doc);
+    $returnSize = trans_byte($size);
+    echo $returnSize;
+
+    //第一种
+    $set_charset = 'export LANG=en_US.UTF-8;';
+    $command = "java -jar ~/Downloads/jodconverter-2.2.2/lib/jodconverter-cli-2.2.2.jar {$doc}   {$pdf}";
+    $t1 = microtime(true);
+
+    //exec 参数说明
+    //  command 执行命令行
+    //  output  执行的结果
+    //  status  执行的状态
+    exec($set_charset . $command, $output, $return_var);
+
+    var_dump($set_charset . $command);
+    var_dump($output);
+    var_dump($return_var);
+
+
+
+    //第二种
+    /*$command = "/Applications/calibre.app/Contents/calibre-debug.app/Contents/MacOS/ebook-convert {$doc}   {$pdf}";
+    $t1 = microtime(true);
+
+    //exec 参数说明
+    //  command 执行命令行
+    //  output  执行的结果
+    //  status  执行的状态
+    exec( $command, $output, $return_var);
+
+    var_dump($command);
+    var_dump($output);
+    var_dump($return_var);*/
+
+
+
+    $t2 = microtime(true);
+    echo '耗时'.round($t2-$t1,3).'秒<br>';
+    echo 'Now memory_get_usage: ' . memory_get_usage() . '<br />';
+
+}
+
+function trans_byte($byte)
+
+{
+
+    $KB = 1024;
+
+    $MB = 1024 * $KB;
+
+    $GB = 1024 * $MB;
+
+    $TB = 1024 * $GB;
+
+    if ($byte < $KB) {
+
+        return $byte . "B";
+
+    } elseif ($byte < $MB) {
+
+        return round($byte / $KB, 2) . "KB";
+
+    } elseif ($byte < $GB) {
+
+        return round($byte / $MB, 2) . "MB";
+
+    } elseif ($byte < $TB) {
+
+        return round($byte / $GB, 2) . "GB";
+
+    } else {
+
+        return round($byte / $TB, 2) . "TB";
+
+    }
+
+}
